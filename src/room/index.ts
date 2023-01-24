30134077593a5b3d40b0102e1a826eccbaeacb76
Ana Beatriz de Almeida <97922588+anabeatrizalmeida@users.noreply.github.com>
2023-01-24 18:06:01 -0300

    fix video

--- src/room/index.ts
+++ src/room/index.ts
@@ -1,20 +1,10 @@
 import { Socket } from "socket.io";
 import { v4 as uuidV4 } from "uuid";
-
-const rooms: Record<string, Record<string, IUser>> = {};
+const rooms: Record<string, string[]> = {};
 const chats: Record<string, IMessage[]> = {};
-
-interface IUser {
-  peerId: string;
-  userName: string;
-}
 interface IRoomParams {
   roomId: string;
   peerId: string;
-}
-
-interface IJoinRoomParams extends IRoomParams {
-  userName: string;
 }
 
 interface IMessage {
@@ -26,19 +16,19 @@ interface IMessage {
 export const roomHandler = (socket: Socket) => {
   const createRoom = () => {
     const roomId = uuidV4();
-    rooms[roomId] = {};
+    rooms[roomId] = [];
     socket.emit("room-created", { roomId });
     console.log("user created the room");
   };
-
-  const joinRoom = ({ roomId, peerId, userName }: IJoinRoomParams) => {
-    if (!rooms[roomId]) rooms[roomId] = {};
+  const joinRoom = ({ roomId, peerId }: IRoomParams) => {
+    if (!rooms[roomId]) rooms[roomId] = [];
     if (!chats[roomId]) chats[roomId] = [];
+    
     socket.emit("get-messages", chats[roomId]);
-    console.log("user joined the room", roomId, peerId, userName);
-    rooms[roomId][peerId] = { peerId, userName };
+    console.log("user joined the room", roomId, peerId);
+    rooms[roomId].push(peerId);
     socket.join(roomId);
-    socket.to(roomId).emit("user-joined", { peerId, userName });
+    socket.to(roomId).emit("user-joined", { peerId });
     socket.emit("get-users", {
       roomId,
       participants: rooms[roomId],
@@ -51,8 +41,17 @@ export const roomHandler = (socket: Socket) => {
   };
 
   const leaveRoom = ({ peerId, roomId }: IRoomParams) => {
-    // rooms[roomId] = rooms[roomId]?.filter((id) => id !== peerId);
+    rooms[roomId] = rooms[roomId].filter((id) => id !== peerId);
+    socket.to(roomId).emit("user-disconnect", peerId);
     socket.to(roomId).emit("user-disconnected", peerId);
+  };
+
+  const startSharing = ({ peerId, roomId }: IRoomParams) => {
+    socket.to(roomId).emit("user-started-sharing", peerId);
+  };
+
+  const stopSharing = (roomId: string) => {
+    socket.to(roomId).emit("user-stopped-sharing");
   };
 
   const addMessage = (roomId: string, message: IMessage) => {
@@ -65,22 +64,9 @@ export const roomHandler = (socket: Socket) => {
     socket.to(roomId).emit("add-message", message);
   };
 
-  const changeName = ({
-    peerId,
-    userName,
-    roomId,
-  }: {
-    peerId: string;
-    userName: string;
-    roomId: string;
-  }) => {
-    if (rooms[roomId] && rooms[roomId][peerId]) {
-      rooms[roomId][peerId].userName = userName;
-      socket.to(roomId).emit("name-changed", { peerId, userName });
-    }
-  };
   socket.on("create-room", createRoom);
   socket.on("join-room", joinRoom);
+  socket.on("start-sharing", startSharing);
+  socket.on("stop-sharing", stopSharing);
   socket.on("send-message", addMessage);
-  socket.on("change-name", changeName);
 };
